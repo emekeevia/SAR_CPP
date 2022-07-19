ee1678561608defdeb00e01c701d36f154cfd02c
emekeevia <emekeev.ia@phystech.edu>
2022-07-19 19:08:44 +0300

    Add azimuth compression

--- main.cpp
+++ main.cpp
@@ -31,18 +31,18 @@ void SAR(){
     //RANGE DOMAIN
     // Basic definitions
     vector<complex<double>> range_chirp(size_range); //empty vector to be filled with chirp values
-    //vector<std::complex<double>> RANGE_CHIRP(size_range);
     vector<complex<double>> tau = fill_up(-tau_p/2, tau_p/2, 1/fs);  // time axis in range
-    //vector<std::complex<double>> omega = fill_up(-fs/2, fs/2, 1/tau_p);  // frequency axis in range
+
 
     //Define chirp in range
-    vector<std::complex<double>> ra_chirp_temp (tau.size());
-    for(size_t i = 0;i < tau.size();i++){
+    //Get size of chirp
+    size_t size_chirp_r = tau.size();
+    vector<std::complex<double>> ra_chirp_temp (size_chirp_r);
+    for(size_t i = 0;i < size_chirp_r;i++){
         ra_chirp_temp[i] = exp(tau[i]*tau[i]*M_PI*K_r*static_cast<complex<double>>(I));
     }
 
-    //Get size of chirp
-    size_t size_chirp_r = tau.size();
+
 
     //Position chirp in range vector (centered)
     // --> used for correlation procedure
@@ -65,9 +65,57 @@ void SAR(){
     vector<complex<double>> CON_RANGE_CHIRP(size_range);
     conjugate(CON_RANGE_CHIRP, RANGE_CHIRP);
 
+    //-------------------------------------------------------------------------------------------------------
+
+    //AZIMUTH DOMAIN
+
+    // E.) Define correlation chirp in azimuth
+
+    // Basic definitions
+    vector<complex<double>> azimuth_chirp(size_azimuth);  // empty vector to be filled with chirp values
+    vector<complex<double>> t = fill_up(-ta/2, ta/2, 1/prf);// time axis in azimuth
+
+
+    // FM Rate Azimuth Chirp
+    double K_a = (-2*V*V)/(Lambda*R_0);
+
+    // Define chirp in azimuth
+    // Get size of chirp
+    size_t size_chirp_a = t.size();
+    vector<std::complex<double>> az_chirp_temp (size_chirp_a);
+    for(size_t i = 0;i < size_chirp_a;i++){
+        ra_chirp_temp[i] = exp(t[i]*t[i]*M_PI*K_a*static_cast<complex<double>>(I));
+    }
+
+
+
+
+    // Position chirp in azimuth vector (centered)
+    // --> used for correlation procedure
+    az_chirp_temp.insert(az_chirp_temp.begin(),size_azimuth - size_chirp_a,0);
+    azimuth_chirp = az_chirp_temp;
+
+    // Transform vector in frequency domain (Fourier transform)
+    fftw_plan plan;
+    vector<complex<double>> AZIMUTH_CHIRP(size_azimuth);
+
+
+    plan = fftw_plan_dft_1d(size_azimuth, reinterpret_cast<fftw_complex*>(&azimuth_chirp),
+                              reinterpret_cast<fftw_complex*>(&AZIMUTH_CHIRP), FFTW_FORWARD, FFTW_ESTIMATE); //making draft plan
+    fftw_execute(plan); // Fourier Transform
+    fftw_destroy_plan(plan);
+
+    // Define chirp for correlation
+    // --> conjugate complex of signal chirp
+    vector<complex<double>> CON_AZIMUTH_CHIRP(size_azimuth);
+    conjugate(CON_AZIMUTH_CHIRP, AZIMUTH_CHIRP);
+
+
+    //-------------------------------------------------------------------------------------------------------
     vector<vector<complex<double>>>processed(size_azimuth, vector<complex<double>>(size_range));
 
-
+    //Focusing
+    //1)Range compression
     for(size_t k1 = 0; k1 < size_azimuth;k1++) {
         size_t size = Raw_data[k1].size();
         vector<complex<double>> vek_out(size_range);
@@ -79,11 +127,36 @@ void SAR(){
         vector<complex<double>> if_vek(size_range);
         plan = fftw_plan_dft_1d(size_range, reinterpret_cast<fftw_complex*>(&CORR),
                                            reinterpret_cast<fftw_complex*>(&if_vek), FFTW_BACKWARD, FFTW_ESTIMATE);// Inverse Fourier Transform
+        fftw_execute(plan);
 
         iFFTshift(if_vek);// Sorting after FFT
         processed [k1] = if_vek;  // Store row in matrix
         fftw_destroy_plan(plan);
     }
+    //2)Azimuth compression
+    for(size_t k2 = 0; k2 < size_range;k2++) {
+        vector<complex<double>> vek2(size_azimuth);// = processed[:, k2];  // Select row in azimuth
+        for(size_t i = 0; i < size_azimuth;i++){
+            vek2[i] = processed[i][k2];
+        }
+        vector<complex<double>> VEK2(size_azimuth);
+        fftw_plan plan = fftw_plan_dft_1d(size_azimuth, reinterpret_cast<fftw_complex*>(&vek2),
+                                          reinterpret_cast<fftw_complex*>(&VEK2), FFTW_FORWARD, FFTW_ESTIMATE);//making draft plan
+        fftw_execute(plan); // Fourier Transform
+        vector<complex<double>> CORR2 = VEK2*CON_AZIMUTH_CHIRP;
+
+        vector<complex<double>> if_vek2(size_azimuth);
+        plan = fftw_plan_dft_1d(size_range, reinterpret_cast<fftw_complex*>(&CORR2),
+                                reinterpret_cast<fftw_complex*>(&if_vek2), FFTW_BACKWARD, FFTW_ESTIMATE);// Inverse Fourier Transform
+        fftw_execute(plan);
+
+        iFFTshift(if_vek2);// Sorting after FFT
+        for(size_t i = 0; i < size_azimuth;i++){
+            processed[i][k2] = if_vek2[i];
+        }
+        fftw_destroy_plan(plan);
+    }
+
 }
 
 int main() {
